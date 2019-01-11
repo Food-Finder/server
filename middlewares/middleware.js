96983e708839ec9060a32322b5b7a705e9741408
Atras Tudhi <atras.r@gmail.com>
2019-01-11 15:25:58 +0700

    middleware

--- middlewares/middleware.js
+++ middlewares/middleware.js
@@ -1,9 +1,14 @@
 const axios = require('axios')
 
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client(process.env.CLIENT_ID);
+
 module.exports = {
   googleSign: (req, res, next) => {
+    console.log(req.body);
     axios.get(`https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${req.body.token}`)
     .then(({ data }) => {
+      console.log(data)
       if(data.aud !== process.env.CLIENT_ID) {
         res.status(500).json({
           message: 'client id is not valid'
@@ -14,9 +19,10 @@ module.exports = {
       }
     })
     .catch(err => {
+      console.log(err)
       res.status(500).json({
         errors: err
       })
     })
-  }  
+  }
 }
